0a5c9ef08c6921d1dd52c83a091ce7ff13047911
someshsingh22 <someshsingh9414@gmail.com>
2020-06-13 05:16:58 +0530

    First scrape

--- tweets.py
+++ tweets.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 import GetOldTweets3 as got
-import time
-import json
-searches=["@PMOIndia", "@RahulGandhi", "@ArvindKejriwal", "#कमीने", '#कुत्ते', '#नीच']
+import time, json, pickle
+from datetime import datetime, date, timedelta
+
+searches=["भेनचोद","भोसडीके","चुदाई","कुत्ते","मुल्ले","दलाल","नीच","गाँड","हरामी","रंडी","सूअर","भड़वा","गांड","लुंड","माधरचोद","मादरचोद","बहनचोद","भेनचोद","#भोसडीके","#चुदाई","#कुत्ते","#मुल्ले","#दलाल","#नीच","#गाँड","#हरामी","#रंडी","#सूअर","#भड़वा","#गांड","#लुंड","#माधरचोद","#मादरचोद","#बहनचोद"]
+query = " OR ".join(searches)
 results = []
 
-for query in searches:
-    tweetCriteria = got.manager.TweetCriteria().setLang("hi").setQuerySearch(query).setMaxTweets(10)
-    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-    for tweet in tweets:
-        results.append(vars(tweet))
-    time.sleep(2)
-    print(query)
-
-with open('out.txt', 'w+') as fout:
-    fout.write('\n'.join(str(results).split('{')))
+def DownloadTweets(SinceDate, UntilDate, Query, update_days=30, max_tweets=1000, sleep=300):
+    '''
+    Downloads all tweets from a certain month in three sessions in order to avoid sending too many requests. 
+    Date format = 'yyyy-mm-dd'. 
+    Query=string.
+    '''
+    since = datetime.strptime(SinceDate, '%Y-%m-%d')
+    until= datetime.strptime(UntilDate, '%Y-%m-%d')
+    current = since
+    results = []
+    while(current < until):
+        batch_criteria = got.manager.TweetCriteria().setQuerySearch(Query).setSince(current.strftime('%Y-%m-%d')).setUntil((current + timedelta(days = update_days)).strftime('%Y-%m-%d')).setLang('hi').setMaxTweets(max_tweets)
+        current = current + timedelta(days = update_days)
+        batch_tweets = got.manager.TweetManager.getTweets(batch_criteria)
+        results.extend(batch_tweets)
+        print("Retrieving tweets from :" + current.strftime('%Y-%m-%d'), " to ", (current + timedelta(days = update_days)).strftime('%Y-%m-%d'))
+        time.sleep(sleep)
+    return results
